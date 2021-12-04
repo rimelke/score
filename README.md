ee1578f9b7a640337efb7b386d617b00ea7a4f02
Rikelme Griep <rikelme.griep@afya.com.br>
2021-12-04 14:50:47 -0300

    update README

--- README.md
+++ README.md
@@ -1,11 +1,81 @@
-# Score
+# **:musical_note: Score**
 
-Este sistema é um webapp de listagem e pré-visualização de músicas do deezer.
+An app to find and get together all of your favourite Deezer' songs.
 
-## Rodando o sistema
+## :bulb: Objective
 
-Para rodar, basta clonar o repositório, instalar as dependências com `npm install` ou `yarn`, entrar na pasta web e rodar o comando `dev`. O sistema iniciará na porta **3000**.
+This application was build to be a place where you can search for specific songs, see the top listeneds and favorite them.
 
-## Acessando o sistema
+## :heavy_check_mark: Features
 
-Para acessar basta acessar a url [http://localhost:3000](http://localhost:3000) no seu navegador (depois de ter rodado o sistema) ou [acessar a demo](https://score-tan.vercel.app/).
+### :computer: Web
+
+- Search of songs
+
+- Favorite list
+
+- Play a short demo
+
+## :art: Layout
+
+<h1 align="center">
+    <img alt="Layout" title="#Layout" src="./assets/layout.png" />
+</h1>
+
+## **:wrench: Technologies**
+
+### :computer: Website
+
+- [Next][next]
+- [Redux][redux]
+- [Styled-Components][styled]
+
+## :books: How to use
+
+:computer: **Website**
+
+```sh
+
+# Start the website
+
+
+$ npm run dev
+
+or
+
+$ yarn dev
+
+
+
+# The app will be running on port 3000
+
+```
+
+## :open_book: How to contribute
+
+1. Make a **fork**.
+
+2. Make a new branch with your changes: `git checkout -b my-feature`
+
+3. Save your changes and make a commit with a cool message: `git commit -m "feat: My new feature"`
+
+4. Send your changes: `git push origin my-feature`
+
+> If a question persists, see [how to contribute in GitHub](https://github.com/firstcontributions/first-contributions)
+
+## :memo: License
+
+This project is under the **MIT License**. See [LICENSE][license] to know more.
+
+---
+
+<h4  align="center">
+
+Made with ❤️ by <a  href="https://www.linkedin.com/in/rikelme-griep-b265a51ab"  target="_blank">Rikelme Griep</a>
+
+</h4>
+
+[license]: https://opensource.org/licenses/MIT
+[next]: https://nextjs.org/
+[redux]: https://redux.js.org/
+[styled]: https://styled-components.com/
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,93 +1,99 @@
-import axios from "axios"
-import { GetStaticProps } from "next"
-import { useState } from "react"
-import styled from "styled-components"
-import TrackList from "../components/TrackList"
-import api from "../services/api"
-import Link from 'next/link'
+import axios from "axios";
+import { GetStaticProps } from "next";
+import { useState } from "react";
+import styled from "styled-components";
+import TrackList from "../components/TrackList";
+import api from "../services/api";
+import Link from "next/link";
 
 const Header = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 3rem;
+
+  div {
+    flex: 1;
     display: flex;
-    justify-content: space-between;
+    flex-direction: column;
+    justify-content: center;
     align-items: center;
-    margin-bottom: 3rem;
+  }
 
-    div {
-        flex: 1;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center
-    }
+  img {
+    width: 14rem;
+    align-self: flex-start;
+  }
 
-    img {
-        width: 14rem;
-        align-self: flex-start;
-    }
+  input {
+    padding: 0.4rem 0.8rem;
+    border-radius: 0;
+    border: 1px solid #696969;
+    outline: none;
+    font-size: 0.9rem;
+    width: 70%;
+    text-align: center;
+  }
 
-    input {
-        padding: .4rem .8rem;
-        border-radius: 0;
-        border: 1px solid #696969;
-        outline: none;
-        font-size: .9rem;
-        width: 70%;
-        text-align: center;
-    }
+  a {
+    align-self: flex-end;
+    color: black;
+    text-decoration: none;
+  }
+`;
 
-    a {
-        align-self: flex-end;
-        color: black;
-        text-decoration: none;
-    }
-`
+const Home = (props) => {
+  const [tracks, setTracks] = useState(props.tracks);
 
-const Home = props => {
-    const [tracks, setTracks] = useState(props.tracks)
+  function search(value: string) {
+    if (value !== "")
+      axios
+        .get(`/api/search?q=${value}`)
+        .then((res) => setTracks(res.data.data));
+    else setTracks(props.tracks);
+  }
 
-    function search(value: string) {
-        if (value !== '')
-            axios.get(`/api/search?q=${value}`)
-            .then(res => setTracks(res.data.data))
-        else
-            setTracks(props.tracks)
-    }
+  return (
+    <>
+      <Header>
+        <div>
+          <img src="logo.svg" alt="Logo" />
+        </div>
+        <div>
+          <input
+            placeholder="Search for something"
+            type="text"
+            onChange={(e) => search(e.target.value)}
+          />
+        </div>
+        <div>
+          <Link href="/favorites">Your favourites</Link>
+        </div>
+      </Header>
+      <TrackList
+        tracks={tracks.map((track) => ({
+          artist: track.artist.name,
+          audio: track.preview,
+          duration: track.duration,
+          image: track.album.cover,
+          title: track.title_short,
+          id: track.id,
+          link: track.link,
+        }))}
+      />
+    </>
+  );
+};
 
-    return (
-        <>
-            <Header>
-                <div>
-                    <img src="logo.svg" alt="Logotipo"/>
-                </div>
-                <div>
-                    <input placeholder="Pesquise por algo" type="text" onChange={e => search(e.target.value)}/>
-                </div>
-                <div>
-                    <Link href="/favorites">Suas favoritas</Link>
-                </div>
-            </Header>
-            <TrackList tracks={tracks.map(track => ({
-                artist: track.artist.name,
-                audio: track.preview,
-                duration: track.duration,
-                image: track.album.cover,
-                title: track.title_short,
-                id: track.id,
-                link: track.link
-            }))} />
-        </>
-    )
-}
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  const { data } = await api.get("/chart/0/tracks?limit=15");
 
-export const getStaticProps: GetStaticProps = async ctx => {
-    const { data } = await api.get('/chart/0/tracks?limit=15')
+  return {
+    props: {
+      tracks: data.data,
+    },
+    revalidate: 3600,
+  };
+};
 
-    return {
-        props: {
-            tracks: data.data
-        },
-        revalidate: 3600
-    }
-}
-
-export default Home
+export default Home;
